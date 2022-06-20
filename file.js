e6fe2571f4fad10e2824b7ee1827ae753adee45f
maha-ghariani <mahaghariani505@gmail.com>
2022-06-20 20:36:23 +0100

    second commit

--- file.js
+++ file.js
@@ -4,6 +4,7 @@ const qtOfItem=document.querySelectorAll('.qt');
 const totals=document.querySelector('#totalsum');
 const priceofItem=document.querySelectorAll('.price');
 const reseat=document.querySelectorAll('.delete');
+const like=document.querySelectorAll('.like');
 
 const bigdivcontent=document.querySelectorAll('.card');
 console.log(bigdivcontent);
@@ -11,7 +12,7 @@ for(let i=0;i<buttonMoins.length;i++){
     buttonMoins[i].addEventListener('click',function(){
         if(qtOfItem[i].innerHTML>0){
             qtOfItem[i].innerHTML--;
-      totals.textContent=parseInt(totals.textContent)+parseInt(priceofItem[i].textContent);
+      totals.textContent=parseInt(totals.textContent)-parseInt(priceofItem[i].textContent);
         }
     }
 )};
@@ -28,4 +29,10 @@ for(let i=0;i<buttonplus.length;i++){
         totals.innerHTML=parseInt(totals.innerHTML)-parseInt(priceofItem[i].innerHTML)*parseInt(qtOfItem[i].innerHTML);
      });
  }
- 
+ for(let i=0;i<like.length;i++)
+ {
+    like[i].addEventListener('click',function(){
+        like[i].style.filter='invert(80%)';
+        like[i].style.filter='invert(61%) sepia(73%) saturate(7472%) hue-rotate(344deg) brightness(97%) contrast(90%)';
+    })
+ }
